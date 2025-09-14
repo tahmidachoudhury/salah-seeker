4537a6f7064e450f3873f101490c8deed2b679b7
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2025-09-14 14:12:57 +0100

    UPDATE: bundle now running inside dev build

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,20 +3,20 @@ import MapboxGL from "@rnmapbox/maps";
 import Constants from "expo-constants";
 import { useEffect } from "react";
 import { ErrorUtils } from "react-native";
+import { setJSExceptionHandler } from "react-native-exception-handler";
 
 // setting the token early, pulled from app config
 MapboxGL.setAccessToken(Constants.expoConfig?.extra?.mapboxAccessToken);
 
 export default function RootLayout() {
   useEffect(() => {
-    // Set up global JS error handler
-    ErrorUtils.setGlobalHandler((error, isFatal) => {
-      console.error("🔥 Global error caught:", error);
+    setJSExceptionHandler((error, isFatal) => {
+      console.error("🔥 Global JS Error:", error);
 
       if (isFatal) {
         alert(`Fatal JS error: ${error.message}`);
       }
-    });
+    }, true); // <- second arg = allow in dev mode
   }, []);
 
   return <Stack />;
